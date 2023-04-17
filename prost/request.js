043318daa8a175d90ca29d2747d731987db2df1a
rahaf <rahafmuradali@gmail.com>
2023-04-17 06:34:45 +0300

    creat new project

---
+++ prost/request.js
@@ -0,0 +1,43 @@
+function request(ani){
+	switch(ani){
+case 1:alert("Your requst  for adoption (OLLIE) has been successfully submitted");
+break;
+
+case 2:alert("Your requst  for adoption (ZEUS) has been successfully submitted");
+break;
+
+case 3:alert("Your requst  for adoption (Lisa & HANK) has been successfully submitted");
+break;
+
+case 4:alert("Your requst  for adoption (FINN) has been successfully submitted");
+break;
+
+case 5:alert("Your requst  for adoption (THEO) has been successfully submitted");
+break;
+
+case 6:alert("Your requst  for adoption (Oreo) has been successfully submitted");
+break;
+
+case 7:alert("Your requst  for adoption (Simba) has been successfully submitted");
+break;
+ 
+case 8:alert("Your requst  for adoption (Lucky) has been successfully submitted");
+break;
+
+case 9:alert("Your requst  for adoption (Angel & Daisy) has been successfully submitted");
+break;
+
+case 10:alert("Your requst  for adoption (Kiwi & Coco & lielie & harfe) has been successfully submitted");
+break;
+ 
+case 11:alert("Your requst  for adoption (BB & jojo) has been successfully submitted");
+break;
+
+case 12:alert("Your requst  for adoption (Bonnie) has been successfully submitted");
+break;
+ 
+case 13:alert("Your requst  for adoption (Remo family) has been successfully submitted");
+break;
+
+	}
+}
---
+++ prost/s.js
@@ -0,0 +1,10 @@
+function request(ani){
+  switch(ani){
+case 1:alert("Your requst  for adoption (OLLIE) has been successfully submitted");
+break;
+
+case 2:alert("Your request for sitting ("hary") has been successfully submitted");
+break;
+
+case 3:alert("Your request for sitting ("Reko") has been successfully submitted");
+break;}}
